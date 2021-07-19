2e653e1f970110e5da06948f3ec001b1beaf7a21
aleh-yaskovich <aleh.yask@gmail.com>
2021-07-19 13:17:34 +0300

    Project with Logger

--- web-app/src/main/java/com/epam/brest/web_app/BeverageController.java
+++ web-app/src/main/java/com/epam/brest/web_app/BeverageController.java
@@ -1,10 +1,14 @@
 package com.epam.brest.web_app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BeverageController {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(BeverageController.class);
 
     /**
      * Show beverage list
