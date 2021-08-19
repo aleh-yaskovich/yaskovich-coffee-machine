d5ef499f54b7db22348962658c1e9c2c59ca5141
aleh-yaskovich <aleh.yask@gmail.com>
2021-08-19 23:13:50 +0300

    Spring boot with different profiles

--- web-app/src/main/java/com/epam/brest/web_app/ApplicationMvcConfig.java
+++ web-app/src/main/java/com/epam/brest/web_app/ApplicationMvcConfig.java
@@ -2,58 +2,25 @@ package com.epam.brest.web_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
-import org.thymeleaf.templatemode.TemplateMode;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 @ComponentScan("com.epam.brest*")
 public class ApplicationMvcConfig {
 
+
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         PropertySourcesPlaceholderConfigurer propertySourcesPlaceholder = new PropertySourcesPlaceholderConfigurer();
         Resource[] resourceLocations = new Resource[] {
-                new ClassPathResource("dao.properties")
+                new ClassPathResource("dao.properties"),
+                new ClassPathResource("application.properties")
         };
         propertySourcesPlaceholder.setLocations(resourceLocations);
         return propertySourcesPlaceholder;
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        EmbeddedDatabase db = new EmbeddedDatabaseBuilder()
-                .generateUniqueName(true)
-                .setType(EmbeddedDatabaseType.H2)
-                .setScriptEncoding("UTF-8")
-                .ignoreFailedDrops(true)
-                .addScripts("create-test-db.sql", "init-test-db.sql")
-                .build();
-        return db;
-    }
-
-    @Bean
-    public JdbcTemplate jdbcTemplate() {
-        return new JdbcTemplate(dataSource());
-    }
-
-    @Bean
-    public NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
-        return new NamedParameterJdbcTemplate(dataSource());
     }
 
     public static void main(String[] args) {
---
+++ web-app/src/main/java/com/epam/brest/web_app/DevDataSourceConfig.java
@@ -0,0 +1,49 @@
+package com.epam.brest.web_app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("dev")
+public class DevDataSourceConfig {
+
+    @Value("${spring.datasource.driver-class-name}")
+    String driverClassName;
+
+    @Value("${spring.datasource.url}")
+    String url;
+
+    @Value("${spring.datasource.username}")
+    String userName;
+
+    @Value("${spring.datasource.password}")
+    String password;
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("create-test-db.sql")
+                .addScript("init-test-db.sql")
+                .build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
+        return new NamedParameterJdbcTemplate(dataSource());
+    }
+}
